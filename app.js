e1cf4b7d93ca7e545edb8a821cb075e01f89233f
kentaro <akiress55555@gmail.com>
2022-08-07 19:32:30 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
-
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-
-
+const express = require('express')
+var mysql = require('mysql')
+const app = express()
 
 const connection =
    mysql.createConnection({
@@ -15,77 +10,16 @@ const connection =
       database:'heroku_00afb7330dc1246'
    });
 
-   connection.query('SELECT * FROM items',(error,results)=>{
+
+var port = process.env.PORT || 3000;
+
+app.get('/',(req, res)=>{
+connection.query('SELECT * FROM items',(error,results)=>{
     res.send('hello'+results[0].name+':'+results[0].text);
       console.log(results)
       console.log(results[0])
       console.log(results[0].name)
   })
+  })
 
-  var serveStatic = require('serve-static');
-  app.use(serveStatic(__dirname + "/dist"));
-
-
-
-
-app.get('/', (req, res) => {
-  res.render('top.ejs');
-});
-
-app.get('/index', (req, res) => {
-  connection.query(
-    'SELECT * FROM items',
-    (error, results) => {
-      res.render('index.ejs', {items: results});
-    }
-  );
-});
-
-app.get('/new', (req, res) => {
-  res.render('new.ejs');
-});
-
-app.post('/create', (req, res) => {
-  connection.query(
-    'INSERT INTO items (name) VALUES (?)',
-    [req.body.itemName],
-    (error, results) => {
-      res.redirect('/index');
-    }
-  );
-});
-
-app.post('/delete/:id', (req, res) => {
-    connection.query(
-        'DELETE FROM items WHERE id = ?',
-    [req.params.id],
-    (error,results) => {
-      res.redirect('/index');
-    }
-        );
-    });
-
-    app.get('/edit/:id', (req, res) => {
-        connection.query(
-            'SELECT * FROM items WHERE id = ?',
-            [req.params.id],
-            (error, results) => {
-        res.render('edit.ejs',{item:results[0]});
-        }
-          );
-        });
-
-        app.post('/update/:id', (req, res) => {
-            connection.query(
-                'UPDATE items SET name = ? WHERE id = ?',
-                [req.body.itemName , req.params.id],
-                (error, results) => {
-                res.redirect('/index');
-                }
-              );
-            });
-
-            const PORT=process.env.PORT || 3000;
-            app.listen(PORT,()=>{
-                console.log("listening server")
-            })
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
