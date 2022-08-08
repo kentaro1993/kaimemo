4f295ea8e91a7095c7ab0fef2d76e706a36686cd
kentaro <akiress55555@gmail.com>
2022-08-08 14:48:25 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -32,3 +32,95 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('heroku-mysql app is running on port', app.get('port'));
 });
+
+
+
+
+
+
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.post('/delete/:id', (req, res) => {
+    connection.query(
+        'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error,results) => {
+      res.redirect('/index');
+    }
+        );
+    });
+
+    app.get('/edit/:id', (req, res) => {
+        connection.query(
+            'SELECT * FROM items WHERE id = ?',
+            [req.params.id],
+            (error, results) => {
+        res.render('edit.ejs',{item:results[0]});
+        }
+          );
+        });
+
+        app.post('/update/:id', (req, res) => {
+            connection.query(
+                'UPDATE items SET name = ? WHERE id = ?',
+                [req.body.itemName , req.params.id],
+                (error, results) => {
+                res.redirect('/index');
+                }
+              );
+            });
+
+
+
+            connection.connect(function(err) {
+              if (err) {
+                  console.log('ERROR.CONNECTION_DB: ', err);
+                  setTimeout(handleDisconnect, 1000);
+              }
+          });
+          
+          connection.on('error', function(err) {
+              console.log('ERROR.DB: ', err);
+              if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                  console.log('ERROR.CONNECTION_LOST: ', err);
+                  handleDisconnect();
+              } else {
+                  throw err;
+              }
+          });
+      
+
+
+
+app.listen(process.env.PORT || 3000);
