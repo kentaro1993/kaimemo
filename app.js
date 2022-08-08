e3f458b82c1a6b7440400b265b37d1957ee8d619
kentaro <akiress55555@gmail.com>
2022-08-08 21:31:21 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
+var express = require('express');
+var mysql = require('mysql2');
+var app = express();
+
+
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-const connection = mysql.createConnection({
-  host: 'us-cdbr-east-06.cleardb.net',
+var db_config = {
+    host: 'us-cdbr-east-06.cleardb.net',
   user: 'b30232f422ebcf',
   password: '4d1d89e0',
   database: 'heroku_00afb7330dc1246'
-});
+};
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
@@ -69,34 +74,48 @@ app.post('/delete/:id', (req, res) => {
 
 
 
-
-            var express = require('express');
-            var mysql = require('mysql2');
-            var app = express();
-            
-            
-            var pool = mysql.createPool(db_config);
-            
             app.set('port', (process.env.PORT || 5000));
+
+            var connection;
+            
+            function handleDisconnect() {
+                console.log('INFO.CONNECTION_DB: ');
+                connection = mysql.createConnection(db_config);
+                
+                connection.connect(function(err) {
+                    if (err) {
+                        console.log('ERROR.CONNECTION_DB: ', err);
+                        setTimeout(handleDisconnect, 1000);
+                    }
+                });
+                
+                connection.on('error', function(err) {
+                    console.log('ERROR.DB: ', err);
+                    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                        console.log('ERROR.CONNECTION_LOST: ', err);
+                        handleDisconnect();
+                    } else {
+                        throw err;
+                    }
+                });
+            }
+            
+            handleDisconnect();
             
             app.get('/', function(request, response) {
-                console.log("heroku-mysql!!");
-                pool.getConnection(function(err, connection){
-                    connection.query('SELECT * FROM items WHERE id = ?', function(err, rows, fields){
+                connection.query('SELECT * FROM items WHERE id = ?', function(err, rows, fields){
                     if(err){
-                        console.log('error: ', err);
+                        console.log('ERROR.SELECT_DB: ', err);
                         throw err;
                     }
                     response.writeHead(200,{'Content-Type': 'text/plain'});
                     response.write(rows[0].message);
                     response.end();
-                    connection.release();
-                    });
                 });
             });
             
             app.listen(app.get('port'), function() {
-              console.log('heroku-mysql app is running on port', app.get('port'));
+                console.log('heroku-mysql app is running on port', app.get('port'));
             });
 
 
