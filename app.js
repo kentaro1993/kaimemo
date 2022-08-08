2bbc1114f061fa6561558b9428178973e21485f8
kentaro <akiress55555@gmail.com>
2022-08-08 14:55:07 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
-var express = require('express');
-var mysql = require('mysql');
-var app = express();
-
-var db_config = {
-    host: 'us-cdbr-east-06.cleardb.net',
-    user: 'b30232f422ebcf',
-    password: '4d1d89e0',
-    database: 'heroku_00afb7330dc1246'
-};
-
-var pool = mysql.createPool(db_config);
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-    console.log("heroku-mysql!!");
-    pool.getConnection(function(err, connection){
-        connection.query('SELECT * FROM items WHERE id=1', function(err, rows, fields){
-        if(err){
-            console.log('error: ', err);
-            throw err;
-        }
-        response.writeHead(200,{'Content-Type': 'text/plain'});
-        response.write(rows[0].message);
-        response.end();
-        connection.release();
-        });
-    });
-});
-
-app.listen(app.get('port'), function() {
-  console.log('heroku-mysql app is running on port', app.get('port'));
-});
-
-
-
-
-
-
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-06.cleardb.net',
+  user: 'b30232f422ebcf',
+  password: '4d1d89e0',
+  database: 'heroku_00afb7330dc1246'
+});
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
@@ -102,24 +70,29 @@ app.post('/delete/:id', (req, res) => {
             });
 
 
-
-            connection.connect(function(err) {
-              if (err) {
-                  console.log('ERROR.CONNECTION_DB: ', err);
-                  setTimeout(handleDisconnect, 1000);
-              }
-          });
-          
-          connection.on('error', function(err) {
-              console.log('ERROR.DB: ', err);
-              if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-                  console.log('ERROR.CONNECTION_LOST: ', err);
-                  handleDisconnect();
-              } else {
-                  throw err;
-              }
-          });
       
+
+
+          var handleKFDisconnect = function() {
+            kfdb.on('error', function(err) {
+                if (!err.fatal) {
+                    return;
+                }
+                if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+                    console.log("PROTOCOL_CONNECTION_LOST");
+                    throw err;
+                }
+                log.error("The database is error:" + err.stack);
+        
+                kfdb = mysql.createConnection(kf_config);
+        
+                console.log("kfid");
+        
+                console.log(kfdb);
+                handleKFDisconnect();
+            });
+           };
+           handleKFDisconnect();
 
 
 
