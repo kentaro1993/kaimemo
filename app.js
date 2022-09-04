8b88a8a7f98fabab05ed7e3b2f6d80dbb6711a3a
kentaro <akiress55555@gmail.com>
2022-09-04 21:46:14 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,73 +1,76 @@
-"use strict";
-const mysql = require('mysql2');
-const util = require('util');
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
-const mySqlPoolOptions = {
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
   host: 'us-cdbr-east-06.cleardb.net',
   user: 'b30232f422ebcf',
   password: '4d1d89e0',
   database: 'heroku_00afb7330dc1246'
-};
-
-async function insertUserAsync(name) {
-  const pool = mysql.createPool(mySqlPoolOptions);
-  pool.query = util.promisify(pool.query);
-  try {
-    const res = await pool.query('INSERT INTO items (name) VALUES (?)', [name]);
-    return res.affectedRows == 1;
-  } catch (err) {
-    console.log(err);
-    return false;
-  } finally {
-    pool.end();
-  }
-}
-
-async function selectUsersAsync() {
-  const pool = mysql.createPool(mySqlPoolOptions);
-  pool.query = util.promisify(pool.query);
-  try {
-    const rows = await pool.query('SELECT * FROM items');
-    for (let row of rows) {
-      console.log(`${row.id} : ${row.name}`);
-    }
-  } catch (err) {
-    console.log(err);
-  } finally {
-    pool.end();
-  }
-}
-
-insertUserAsync('user 003').then(result => {
-  console.log(result);
-  return selectUsersAsync();
-}).then(() => {
-  console.log("finish");
 });
 
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+app.post('/delete/:id', (req, res) => {
+    connection.query(
+        'DELETE FROM items WHERE id = ?',
+    [req.params.id],
+    (error,results) => {
+      res.redirect('/index');
+    }
+        );
+    });
+
+    app.get('/edit/:id', (req, res) => {
+        connection.query(
+            'SELECT * FROM items WHERE id = ?',
+            [req.params.id],
+            (error, results) => {
+        res.render('edit.ejs',{item:results[0]});
+        }
+          );
+        });
+
+        app.post('/update/:id', (req, res) => {
+            connection.query(
+                'UPDATE items SET name = ? WHERE id = ?',
+                [req.body.itemName , req.params.id],
+                (error, results) => {
+                res.redirect('/index');
+                }
+              );
+            });
 
 
 
-function handleDisconnect() {
-  console.log('INFO.CONNECTION_DB: ');
-  connection = mysql.createConnection(db_config);
-  
-  //connection取得
-  connection.connect(function(err) {
-      if (err) {
-          console.log('ERROR.CONNECTION_DB: ', err);
-          setTimeout(handleDisconnect, 1000);
-      }
-  });
-  
-  //error('PROTOCOL_CONNECTION_LOST')時に再接続
-  connection.on('error', function(err) {
-      console.log('ERROR.DB: ', err);
-      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-          console.log('ERROR.CONNECTION_LOST: ', err);
-          handleDisconnect();
-      } else {
-          throw err;
-      }
-  });
-}
+
+
+app.listen(process.env.PORT || 3000);
