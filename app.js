1065e58cb5cb0d04765ae4a0fe5aa6e571e18a9b
kentaro <akiress55555@gmail.com>
2022-09-04 21:27:00 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,76 +1,100 @@
+"use strict";
+const mysql = require('mysql2');
+const util = require('util');
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-
-const connection = mysql.createConnection({
+const mySqlPoolOptions = {
   host: 'us-cdbr-east-06.cleardb.net',
   user: 'b30232f422ebcf',
   password: '4d1d89e0',
   database: 'heroku_00afb7330dc1246'
-});
+};
 
-app.get('/', (req, res) => {
-  res.render('top.ejs');
-});
+async function insertUserAsync(name) {
+  const pool = mysql.createPool(mySqlPoolOptions);
+  pool.query = util.promisify(pool.query);
+  try {
+    const res = await pool.query('INSERT INTO items (name) VALUES (?)', [name]);
+    return res.affectedRows == 1;
+  } catch (err) {
+    console.log(err);
+    return false;
+  } finally {
+    pool.end();
+  }
+}
 
-app.get('/index', (req, res) => {
-  connection.query(
-    'SELECT * FROM items',
-    (error, results) => {
-      res.render('index.ejs', {items: results});
+async function selectUsersAsync() {
+  const pool = mysql.createPool(mySqlPoolOptions);
+  pool.query = util.promisify(pool.query);
+  try {
+    const rows = await pool.query('SELECT * FROM items');
+    for (let row of rows) {
+      console.log(`${row.id} : ${row.name}`);
     }
-  );
+  } catch (err) {
+    console.log(err);
+  } finally {
+    pool.end();
+  }
+}
+
+insertUserAsync('user 003').then(result => {
+  console.log(result);
+  return selectUsersAsync();
+}).then(() => {
+  console.log("finish");
 });
 
-app.get('/new', (req, res) => {
-  res.render('new.ejs');
-});
 
-app.post('/create', (req, res) => {
-  connection.query(
-    'INSERT INTO items (name) VALUES (?)',
-    [req.body.itemName],
-    (error, results) => {
-      res.redirect('/index');
-    }
-  );
-});
 
-app.post('/delete/:id', (req, res) => {
-    connection.query(
-        'DELETE FROM items WHERE id = ?',
-    [req.params.id],
-    (error,results) => {
-      res.redirect('/index');
-    }
-        );
-    });
 
-    app.get('/edit/:id', (req, res) => {
-        connection.query(
-            'SELECT * FROM items WHERE id = ?',
-            [req.params.id],
-            (error, results) => {
-        res.render('edit.ejs',{item:results[0]});
+
+
+
+app.set('port', (process.env.PORT || 5000));
+
+var connection;
+
+function handleDisconnect() {
+    console.log('INFO.CONNECTION_DB: ');
+    connection = mysql.createConnection(db_config);
+    
+    //connection取得
+    connection.connect(function(err) {
+        if (err) {
+            console.log('ERROR.CONNECTION_DB: ', err);
+            setTimeout(handleDisconnect, 1000);
         }
-          );
-        });
+    });
+    
+    //error('PROTOCOL_CONNECTION_LOST')時に再接続
+    connection.on('error', function(err) {
+        console.log('ERROR.DB: ', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('ERROR.CONNECTION_LOST: ', err);
+            handleDisconnect();
+        } else {
+            throw err;
+        }
+    });
+}
 
-        app.post('/update/:id', (req, res) => {
-            connection.query(
-                'UPDATE items SET name = ? WHERE id = ?',
-                [req.body.itemName , req.params.id],
-                (error, results) => {
-                res.redirect('/index');
-                }
-              );
-            });
+handleDisconnect();
 
+app.get('/', function(request, response) {
+    connection.query('SELECT * FROM items WHERE id=1', function(err, rows, fields){
+        if(err){
+            console.log('ERROR.SELECT_DB: ', err);
+            throw err;
+        }
+        response.writeHead(200,{'Content-Type': 'text/plain'});
+        response.write(rows[0].message);
+        response.end();
+    });
+});
 
-
-
-
-app.listen(process.env.PORT || 3000);
+app.listen(app.get('port'), function() {
+    console.log('heroku-mysql app is running on port', app.get('port'));
+});
