d8b481b1ac612a143a6e5e48401ad243566d2fa4
kentaro <akiress55555@gmail.com>
2022-09-04 21:35:13 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 "use strict";
 const mysql = require('mysql2');
 const util = require('util');
-const express = require('express');
-const app = express();
 
 const mySqlPoolOptions = {
   host: 'us-cdbr-east-06.cleardb.net',
@@ -50,51 +48,26 @@ insertUserAsync('user 003').then(result => {
 
 
 
-
-
-
-app.set('port', (process.env.PORT || 5000));
-
-var connection;
-
 function handleDisconnect() {
-    console.log('INFO.CONNECTION_DB: ');
-    connection = mysql.createConnection(db_config);
-    
-    //connection取得
-    connection.connect(function(err) {
-        if (err) {
-            console.log('ERROR.CONNECTION_DB: ', err);
-            setTimeout(handleDisconnect, 1000);
-        }
-    });
-    
-    //error('PROTOCOL_CONNECTION_LOST')時に再接続
-    connection.on('error', function(err) {
-        console.log('ERROR.DB: ', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            console.log('ERROR.CONNECTION_LOST: ', err);
-            handleDisconnect();
-        } else {
-            throw err;
-        }
-    });
+  console.log('INFO.CONNECTION_DB: ');
+  connection = mysql.createConnection(db_config);
+  
+  //connection取得
+  connection.connect(function(err) {
+      if (err) {
+          console.log('ERROR.CONNECTION_DB: ', err);
+          setTimeout(handleDisconnect, 1000);
+      }
+  });
+  
+  //error('PROTOCOL_CONNECTION_LOST')時に再接続
+  connection.on('error', function(err) {
+      console.log('ERROR.DB: ', err);
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+          console.log('ERROR.CONNECTION_LOST: ', err);
+          handleDisconnect();
+      } else {
+          throw err;
+      }
+  });
 }
-
-handleDisconnect();
-
-app.get('/', function(request, response) {
-    connection.query('SELECT * FROM items WHERE id=1', function(err, rows, fields){
-        if(err){
-            console.log('ERROR.SELECT_DB: ', err);
-            throw err;
-        }
-        response.writeHead(200,{'Content-Type': 'text/plain'});
-        response.write(rows[0].message);
-        response.end();
-    });
-});
-
-app.listen(app.get('port'), function() {
-    console.log('heroku-mysql app is running on port', app.get('port'));
-});
