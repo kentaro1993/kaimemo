cf77af682d6d98ae60f9ff6981640213f728778b
kentaro <akiress55555@gmail.com>
2022-09-04 23:04:45 +0900

    任意のコメント

--- app.js
+++ app.js
@@ -1,78 +1,46 @@
-const express = require('express');
-const mysql = require('mysql');
-const app = express();
+var mysql = require('mysql');
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+var conf1 = {
+  host     : 'us-cdbr-east-06.cleardb.net',
+  user     : 'b30232f422ebcf',
+  password : '4d1d89e0',
+  database : 'heroku_00afb7330dc1246',
+  connectTimeout : 10000, //タイムアウト(msec)
+  supportBigNumbers : true, //bigint, decimal をサポートする。
+  connectionLimit : 10, //一度に生成する接続インスタンスの数
+  removeNodeErrorCount : 3 //指定した回数以上接続エラーになったら接続選択対象から削除する。
+};
 
-const connection = mysql.createPool({
-  host: 'us-cdbr-east-06.cleardb.net',
-  user: 'b30232f422ebcf',
-  password: '4d1d89e0',
-  database: 'heroku_00afb7330dc1246'
+
+var pool = mysql.createPoolCluster();
+pool.add('MASTER', conf1);
+pool.add('SLAVE1', conf1);
+app.set('pool', pool);
+
+
+router.get('/', function(req, res, next) {
+
+	var app = req.app;
+	var poolCluster = app.get("pool");
+	
+	//接続確認
+	var pool = poolCluster.of('SLAVE*', 'RANDOM');
+	pool.getConnection(function(err, connection) {
+
+		console.log(connection.config.database);
+
+	    if(err != null){
+			//失敗
+			console.log('ERROR');
+			console.log(err);  	
+			return;
+	    }
+
+		console.log('connected');
+
+		//接続を解放する。接続はpoolに戻る。
+		connection.release();
+	});
+	
+	res.send('db');
 });
-
-module.exports = pool;
-
-app.get('/', (req, res) => {
-  res.render('top.ejs');
-});
-
-app.get('/index', (req, res) => {
-  connection.query(
-    'SELECT * FROM items',
-    (error, results) => {
-      res.render('index.ejs', {items: results});
-    }
-  );
-});
-
-app.get('/new', (req, res) => {
-  res.render('new.ejs');
-});
-
-app.post('/create', (req, res) => {
-  connection.query(
-    'INSERT INTO items (name) VALUES (?)',
-    [req.body.itemName],
-    (error, results) => {
-      res.redirect('/index');
-    }
-  );
-});
-
-app.post('/delete/:id', (req, res) => {
-    connection.query(
-        'DELETE FROM items WHERE id = ?',
-    [req.params.id],
-    (error,results) => {
-      res.redirect('/index');
-    }
-        );
-    });
-
-    app.get('/edit/:id', (req, res) => {
-        connection.query(
-            'SELECT * FROM items WHERE id = ?',
-            [req.params.id],
-            (error, results) => {
-        res.render('edit.ejs',{item:results[0]});
-        }
-          );
-        });
-
-        app.post('/update/:id', (req, res) => {
-            connection.query(
-                'UPDATE items SET name = ? WHERE id = ?',
-                [req.body.itemName , req.params.id],
-                (error, results) => {
-                res.redirect('/index');
-                }
-              );
-            });
-
-
-
-
-
-app.listen(process.env.PORT || 3000);
